67ab3be8040e9c5df82024c5f0d88ff4e237b88d
zima99 <pawel.zimonczyk@gmail.com>
2026-01-12 19:01:07 +0100

    Dodanie pola powtorz haslo i bledow pod inputami

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-register',
