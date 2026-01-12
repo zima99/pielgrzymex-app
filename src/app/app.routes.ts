3e49dab7cfd63d944a388dfbac643add7ff31674
zima99 <pawel.zimonczyk@gmail.com>
2026-01-12 19:30:53 +0100

    admin panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
-import { OfferComponent } from './components/offer/offer';
-import { MapComponent } from './components/map/map';
-import { DetailsComponent } from './components/details/details'; // <--- Import
-import { RegisterComponent } from './components/register/register';
 import { LoginComponent } from './components/login/login';
+import { RegisterComponent } from './components/register/register';
+import { MapComponent } from './components/map/map';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel';
 
 export const routes: Routes = [
+  // Strona główna
   { path: '', component: HomeComponent },
-  { path: 'oferta', component: OfferComponent },
-  { path: 'mapa', component: MapComponent },
-  { path: 'pielgrzymka/:id', component: DetailsComponent }, // <--- Nowa ścieżka dynamiczna
-  { path: 'rejestracja', component: RegisterComponent },
+  
+  // Autoryzacja
   { path: 'logowanie', component: LoginComponent },
+  { path: 'rejestracja', component: RegisterComponent },
+  
+  // Mapa
+  { path: 'mapa', component: MapComponent },
+
+  // Panel Admina (Dostępny pod /admin)
+  { path: 'admin', component: AdminPanelComponent },
+
+  // Opcjonalnie: Przekierowanie nieznanych adresów na stronę główną
+  { path: '**', redirectTo: '' }
 ];
